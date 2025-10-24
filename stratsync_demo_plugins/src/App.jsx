cdf2721f1c31e43afaa020fa48815e366284c9a0
stratsync-rohit <rohit@stratsync.ai>
2025-10-24 12:00:02 +0530

    update in app

--- stratsync_demo_plugins/src/App.jsx
+++ stratsync_demo_plugins/src/App.jsx
@@ -55,8 +55,8 @@ export default function App() {
           style={{
             width: 24,
             height: 24,
-            objectFit: "contain",
-            cursor: "pointer",
+            objectFit: "contain"
+            
           }}
         />
         StratSync ChatBot
