25db6da136d15e492f54b29ca2dd7462d8ae5f0e
stratsync-rohit <rohit@stratsync.ai>
2025-10-28 10:56:04 +0530

    new setup

--- stratsync_demo_inbuild_plugins/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import Popup from './popup/Popup'
-import './index.css'
-
-const container = document.getElementById('root')!
-createRoot(container).render(<Popup />)
